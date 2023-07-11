f606712f7c1399a73cd7c991b3eb775f10f24edc
maqsoodahmad-1 <maqsodmaxood75@gmail.com>
2023-07-11 14:14:23 +0530

    Sending a post request to the node server

---
+++ foodDeliveryApp/src/App.jsx
@@ -0,0 +1,14 @@
+import image from './assets/desktopimg1.jpeg'
+
+function App() {
+  return (
+   <>
+   <div class='mx-12 my-8 mr-12'>
+    <img src={image} alt="food iamge" />
+   </div>
+   </>
+
+  );
+}
+
+export default App;
